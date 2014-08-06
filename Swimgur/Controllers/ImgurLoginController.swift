e85f5107885ad7ff11c847079a12f7f2dc972889
Joseph Neuman <finder39@gmail.com>
2014-08-05 20:04:07 -0700

    Controller folder

---
+++ Swimgur/Controllers/ImgurLoginController.swift
@@ -0,0 +1,9 @@
+//
+//  ImgurLoginController.swift
+//  Swimgur
+//
+//  Created by Joseph Neuman on 8/5/14.
+//  Copyright (c) 2014 Joseph Neuman. All rights reserved.
+//
+
+import UIKit
--- Swimgur/Data/DataManager.swift
+++ Swimgur/Data/DataManager.swift
@@ -6,7 +6,6 @@
 //  Copyright (c) 2014 Joseph Neuman. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
 
